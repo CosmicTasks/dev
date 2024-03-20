54b98654f94909cae2f8a8fd00dbd19519683b4c
giostos13 <160799672+giostos13@users.noreply.github.com>
2024-03-19 22:12:00 -0300

    update caixas

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,12 +4,12 @@ import './App.css'
 import Header from './components/pages/Header'
 import Footer from './components/pages/Footer'
 import LandingPage from './components/pages/LandingPage'
-
+import Caixa from './components/Caixa'
 function App() {
 
   return (
     <>
-      <LandingPage />
+    <Caixa/>
     </>
   )
 }
