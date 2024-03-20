fc7f705685ef19de9bd612489e27f0ca284ca876
Alexandre Silva <dev.alexandresilva@gmail.com>
2024-03-20 22:59:57 +0000

    routes aplicadas

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
 import './index.css'
+import LandingPage from './components/pages/LandingPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />
+  },
+  {
+    path: '/app',
+    element: <App />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
