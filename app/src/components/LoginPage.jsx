9b8d0c06588df5020d76487da768ba1336530707
thekidisalright <dev.alexandresilva@gmail.com>
2024-03-21 03:28:40 -0300

    Remove bootstrap import from LoginPage.jsx and add bootstrap import in main.jsx

--- app/src/components/LoginPage.jsx
+++ app/src/components/LoginPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import style from "./LoginPage.module.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import astronauta from "../assets/astronauta.svg";
 
 const LoginPage = () => {
