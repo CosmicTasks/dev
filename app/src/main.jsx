dfb7a69be0d6facf74b66440921189dc8f6ecdca
thekidisalright <dev.alexandresilva@gmail.com>
2024-03-21 00:49:10 -0300

    importar icones do fontawesome

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,26 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { library } from "@fortawesome/fontawesome-svg-core";
 import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom"
-import './index.css'
-import LandingPage from './components/pages/LandingPage.jsx'
+  faCalendarCheck,
+  faMobileScreen,
+  faPalette,
+  faPuzzlePiece,
+  faPenToSquare,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faCalendarCheck,
+  faMobileScreen,
+  faPalette,
+  faPuzzlePiece,
+  faPenToSquare
+);
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import LandingPage from "./components/pages/LandingPage.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <LandingPage />
+    path: "/",
+    element: <LandingPage />,
   },
   {
-    path: '/app',
-    element: <App />
-  }
+    path: "/app",
+    element: <App />,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
