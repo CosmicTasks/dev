1b2e064d6d47b064dd75a7db851a7c533fd92485
thekidisalright <dev.alexandresilva@gmail.com>
2024-03-22 01:26:58 -0300

    novo app.jsx com sidebar importada e modulo css

---
+++ app/src/components/App.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "./app/Sidebar";
+import style from "./App.module.css";
+
+function App() {
+  return (
+    <div className={style.app}>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
