90539685494cdcb86cf959c99381896f3b486b2b
giostos13 <160799672+giostos13@users.noreply.github.com>
2024-03-22 20:48:17 -0300

    update caixas

--- app/src/components/GerenciaCaixa.jsx
+++ app/src/components/GerenciaCaixa.jsx
@@ -10,50 +10,22 @@ function GerenciaCaixa(){
          
        <div  className={Style.caixa}>
         <div className={Style.caixatitulotema}><p className={Style.titulo}>Vocabulário Francês</p></div>
-        <div className={Style.containercaixas}>
-           <div className={Style.containernumeros} >
-           <ul>
-               <li className={Style.numero1}>2</li>
-               <li className={Style.numero2}>0</li>
-               <li className={Style.numero3}>0</li>
-               <li className={Style.numero4}>0</li>
-               <li className={Style.numero5}>0</li>
-               
-
-
-           </ul>
-
-           
-           
-           </div>
-
-           <div className={Style.containericones}>
-
-           <Icon className={Style.icone} icon="majesticons:box-line" width="28" height="32" color='#511ABE'  />
-
-            <Icon  icon="majesticons:box-line" width="28" height="32"  />
-            <Icon icon="majesticons:box-line" width="28" height="32"  />
-            <Icon icon="majesticons:box-line" width="28" height="32"  />
-            <Icon icon="majesticons:box-line" width="28" height="32"  />
-
-           </div>
-
-          
-
-         
-
-          
-
-           </div>
+      
 
            <div className={Style.containerprin}>
 
                <div className={Style.containercont}>
-                  <div className={Style.containernum}><p>0</p></div>
+                  <div className={Style.containernum}><p>2</p></div>
                  <div className={Style.containerbox}><Icon  icon="majesticons:box-line" width="28" height="32"  /></div>
 
               </div>
 
+
+              <div className={Style.containercont}>
+                  <div className={Style.containernum}><p>0</p></div>
+                 <div className={Style.containerbox}><Icon  icon="majesticons:box-line" width="28" height="32"  /></div>
+
+              </div>
 
               <div className={Style.containercont}>
                   <div className={Style.containernum}><p>0</p></div>
@@ -74,7 +46,24 @@ function GerenciaCaixa(){
               </div>
 
            </div>
-          
+
+           <div className={Style.containerdetcaixas}>
+
+            <div className={Style.detcartoes}><p className={Style.textcartoes}>Cartões na caixa: <b className={Style.numcartoes}>2</b> </p></div>
+
+            <div className={Style.containernewcard}>
+
+                <button id={Style.btnnewcard}>Novo cartão</button>
+            </div>
+           </div>
+
+           <div className={Style.containercontcards}> <b className={Style.numcont}>0/2</b> </div>
+
+           <div className={Style.estudarcard}>
+
+            <button id={Style.btnestudar}>Estudar cartões</button>
+           </div>
+        
 
           
 
