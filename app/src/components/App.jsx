86ad71bd9ac0be506a3a99da7a8f03cd45e4d1d8
thekidisalright <dev.alexandresilva@gmail.com>
2024-03-23 01:52:27 -0300

    Add ModalNewList component to App.jsx

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,12 +1,14 @@
 import Sidebar from "./app/Sidebar";
 import style from "./App.module.css";
 import SecSidebar from "./app/SecSidebar";
+import ModalNewList from "./app/modal/ModalNewList";
 
 function App() {
   return (
     <div className={style.app}>
       <Sidebar />
       <SecSidebar />
+      <ModalNewList />
     </div>
   );
 }
