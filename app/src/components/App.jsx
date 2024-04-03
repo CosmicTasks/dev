61b97106c7ebeaa8f28b15b076d6197d61738878
thekidisalright <dev.alexandresilva@gmail.com>
2024-04-02 22:15:05 -0300

    Refactor App component

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -3,13 +3,11 @@ import style from "./App.module.css";
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
 
-
 function App() {
-
   return (
     <div className={style.app}>
-        
-
+      <Sidebar />
+      <Outlet />
     </div>
   );
 }
