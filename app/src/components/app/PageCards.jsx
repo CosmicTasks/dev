354f86513479eebfb0f8910399f162da0f08f84e
thekidisalright <dev.alexandresilva@gmail.com>
2024-04-04 13:46:08 -0300

    Refactor PageCard component styles

--- app/src/components/app/PageCards.jsx
+++ app/src/components/app/PageCards.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./PageCard.module.css";
 import { UilBox, UilEdit } from "@iconscout/react-unicons";
+import logo from "../../assets/logo-cinza.png";
 
 function PageCard() {
+  const [selectedCard, setSelectedCard] = useState(1);
+
   return (
     <>
       <div className={style.caixasBody}>
@@ -49,6 +52,59 @@ function PageCard() {
           </ul>
         </div>
       </div>
+      {selectedCard ? (
+        <div className={style.cardInfo}>
+          <div className={style.cardHeader}>
+            <h1 className={style.cardTitle}>Vocabulário Francês</h1>
+          </div>
+          <div className={style.wrapperNiveis}>
+            <div className={style.nivel}>
+              <span>2</span>
+              <UilBox size="18" color="var(--r7)" />
+            </div>
+            <div className={style.nivel}>
+              <span>0</span>
+              <UilBox size="18" color="var(--c6)" />
+            </div>
+            <div className={style.nivel}>
+              <span>0</span>
+              <UilBox size="18" color="var(--c6)" />
+            </div>
+            <div className={style.nivel}>
+              <span>0</span>
+              <UilBox size="18" color="var(--c6)" />
+            </div>
+            <div className={style.nivel}>
+              <span>0</span>
+              <UilBox size="18" color="var(--c6)" />
+            </div>
+          </div>
+          <div className={style.secHeader}>
+            <h2 className={style.cardSecTitle}>
+              Cartões na caixa: <span className={style.qtdCards}>2</span>
+            </h2>
+            <button className={style.addCard}>Novo cartão</button>
+          </div>
+          <div className={style.wrapperEstudar}>
+            <h1 className={style.qtdEstudar}>0/2</h1>
+            <button className={style.btnEstudar}>Estudar cartões</button>
+          </div>
+          <div className={style.wrapperTabs}>
+            <div className={style.nav}>
+              <button className={`${style.tab} ${style.active}`}>Hoje</button>
+              <button className={style.tab}>Atuais</button>
+              <button className={style.tab}>Concluídas</button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className={style.noCardSelected}>
+          <img className={style.img} src={logo} alt="Logo" />
+          <p className={style.infoText}>
+            Clique em uma caixa para ver mais informações
+          </p>
+        </div>
+      )}
     </>
   );
 }
