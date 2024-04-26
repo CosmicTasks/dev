b8e6dce535c2ef3d173e64d0e045b5023d026f3c
thekidisalright <dev.alexandresilva@gmail.com>
2024-04-26 18:29:06 -0300

    Add useTaskContext hook to handle task operations in app/src/hooks/useTaskContext.jsx

---
+++ app/src/hooks/useTaskContext.jsx
@@ -0,0 +1,10 @@
+import { TaskContext } from "../context/TaskContext";
+import { useContext } from "react";
+
+export const useTaskContext = () => {
+  const context = useContext(TaskContext);
+  if (!context) {
+    throw new Error("useTaskContext must be used within a TaskContextProvider");
+  }
+  return context;
+};
