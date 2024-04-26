17b58910fcd00692dccc72473cbcaf34067a66d1
thekidisalright <dev.alexandresilva@gmail.com>
2024-04-26 14:25:59 -0300

    Refactor userControllers.js to userController.js in backend/routes/users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,7 +5,7 @@ const {
   createUser,
   deleteUser,
   updateUser,
-} = require("../controllers/userControllers");
+} = require("../controllers/userController");
 
 const router = express.Router();
 
