83cfc7044541895a0f5bc321538c0d7c799f26ce
thekidisalright <dev.alexandresilva@gmail.com>
2024-04-26 14:25:00 -0300

    Refactor server.js to add new routes and update MongoDB connection URI

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
 require("dotenv").config();
-const uri = "mongodb+srv://cosmicdb:cosmicpassword@cosmicserver.xz0bish.mongodb.net/?retryWrites=true&w=majority&appName=CosmicServer";
+const uri = "mongodb://localhost:27017";
 const port = 4000;
 
 // Inicializar app express
 const express = require("express");
 const mongoose = require("mongoose");
-const usersRoutes = require("./routes/users");
-const loginRoutes = require("./routes/login");
 const cors = require("cors");
 
 // Criar app express
@@ -23,8 +21,15 @@ app.use((req, res, next) => {
 });
 
 // rotas
+const usersRoutes = require("./routes/users");
+const loginRoutes = require("./routes/login");
+const cadastroRoutes = require("./routes/cadastro");
+const listasRoutes = require("./routes/listas");
+
 app.use("/api/users", usersRoutes);
 app.use("/api/login", loginRoutes);
+app.use("/api/cadastro", cadastroRoutes);
+app.use("/api/listas", listasRoutes);
 
 // connect to db
 mongoose
