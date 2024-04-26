a19de3a1b7b327f41b1f84cf3e4e243503b95582
thekidisalright <dev.alexandresilva@gmail.com>
2024-04-26 14:22:21 -0300

    Add useEffect hook to fetch user and listas data in App.jsx

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,9 +1,41 @@
 import Sidebar from "./app/Sidebar";
 import style from "./App.module.css";
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useEffect } from "react";
+import { useListaContext } from "../hooks/useListaContext";
+import { useUserContext } from "../hooks/useUserContext";
 
 function App() {
+  const { dispatch: listaDispatch } = useListaContext();
+  const { dispatch: userDispatch } = useUserContext();
+
+  useEffect(() => {
+    const userJSON = JSON.parse(localStorage.getItem("user"));
+    console.log(userJSON);
+    const fetchUser = async () => {
+      if (userJSON) {
+        userDispatch({ type: "SET_USER", payload: userJSON });
+      }
+    }
+
+    const fetchListas = async () => {
+      const response = await fetch(`http://localhost:4000/api/listas/${userJSON._id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+      if (response.ok) {
+        listaDispatch({ type: "SET_LISTAS", payload: data });
+      }
+    }
+
+    fetchUser();
+    fetchListas();
+  }, [])
+  
   return (
     <div className={style.app}>
       <Sidebar />
