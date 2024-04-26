2c586f8a621cd752c3b5f9a320c5bf9547a4bf4f
thekidisalright <dev.alexandresilva@gmail.com>
2024-04-26 14:25:31 -0300

    Add flashcardSchema and niveis array to caixaModel.js

--- backend/models/caixaModel.js
+++ backend/models/caixaModel.js
@@ -1,8 +1,37 @@
 const mongoose = require("mongoose");
 
+const flashcardSchema = new mongoose.Schema({
+  frente: {
+    type: String,
+    required: true,
+  },
+  verso: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true })
+
 const caixaSchema = new mongoose.Schema({
   nome: {
     type: String,
     required: true,
   },
-})
+  niveis: [{
+    numero:{
+      type: Number,
+      required: true,
+    },
+    intervalo: {
+      type: Number,
+      required: true,
+    },
+    flashcards: [flashcardSchema],
+    usuario: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  }]
+}, { timestamps: true });
+
+module.exports = mongoose.model("Caixa", caixaSchema);
