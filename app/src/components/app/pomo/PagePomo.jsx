e0a810b60d57cb61a87fe0f082821665a86b1818
mthvalsz <133716976+mthvalsz@users.noreply.github.com>
2024-04-30 22:49:55 -0300

    iniciado a configuração

--- app/src/components/app/pomo/PagePomo.jsx
+++ app/src/components/app/pomo/PagePomo.jsx
@@ -1,11 +1,19 @@
 import style from './PagePomo.module.css'
 import Timer from './Timer'
-
+import Settings from './Settings'
+import { useState } from 'react'
 const PagePomo = () => {
+  
+
+
+  const [showSettings, setShowSettings] = useState(false);
+
   return (
     <div className={style.pagePomo}>
       <div className={style.pomoBody}>
-        <Timer estilo={style} />
+        {showSettings ? <Settings/> :  <Timer estilo={style} />}
+        
+        
       </div>
     </div>
   )
---
+++ app/src/components/app/pomo/Settings.jsx
@@ -0,0 +1,12 @@
+import ReactSlider from 'react-slider';
+function Settings(){
+    return (
+        <div  style={{textAlign:"left"}}>
+            <label>Tempo de concentração:</label>
+
+            <label>Tempo de descanso:</label>
+        </div>
+    )
+}
+
+export default Settings;
