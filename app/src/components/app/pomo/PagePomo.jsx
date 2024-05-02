1946fc77f95ed60ef3befe9049850ca78c55dd75
mthvalsz <133716976+mthvalsz@users.noreply.github.com>
2024-05-02 20:56:31 -0300

    Finalizado o funcionamente do Pomodoro.

--- app/src/components/app/pomo/PagePomo.jsx
+++ app/src/components/app/pomo/PagePomo.jsx
@@ -2,16 +2,29 @@ import style from './PagePomo.module.css'
 import Timer from './Timer'
 import Settings from './Settings'
 import { useState } from 'react'
+import SettingsContext from './SettingsContext'
 const PagePomo = () => {
   
 
 
   const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
 
   return (
     <div className={style.pagePomo}>
       <div className={style.pomoBody}>
-        {showSettings ? <Settings/> :  <Timer estilo={style} />}
+        <SettingsContext.Provider value={{
+          showSettings,
+          setShowSettings,
+          workMinutes,
+          breakMinutes,
+          setWorkMinutes,
+          setBreakMinutes,
+        }}>
+          {showSettings ? <Settings/> :  <Timer estilo={style} />}
+        </SettingsContext.Provider>
+        
         
         
       </div>
--- app/src/components/app/pomo/Timer.jsx
+++ app/src/components/app/pomo/Timer.jsx
@@ -4,19 +4,80 @@ import 'react-circular-progressbar/dist/styles.css';
 import PlayButton from './PlayButton';
 import PauseButton from './PauseButton';
 import SettingsButton from './SettingsButton';
+import { useContext, useState, useEffect, useRef } from 'react';
+import SettingsContext from './SettingsContext';
 
 const Timer = ({estilo}) => {
+  const settingsInfo = useContext(SettingsContext)
+
+  const [isPaused, setIsPaused] = useState(true);
+  const [mode, setMode] = useState('trabalho');
+  const [secondsLeft, setSecondsLeft] = useState(0);
+
+  const secondsLeftRef = useRef(secondsLeft);
+  const isPausedRef = useRef(isPaused);
+  const modeRef = useRef(mode);
+
+  function switchMode() {
+    const nextMode = modeRef.current === 'work' ? 'break' : 'work';
+    const nextSeconds = (nextMode === 'work' ? settingsInfo.workMinutes : settingsInfo.breakMinutes) * 60;
+    setMode(nextMode);
+    modeRef.current = nextMode;
+    setSecondsLeft(nextSeconds);
+    secondsLeftRef.current = nextSeconds;
+
+  }
+
+  function tick() {
+    secondsLeftRef.current--;
+    setSecondsLeft(secondsLeftRef.current);
+  }
+
+  function initTimer() {
+    setSecondsLeft(settingsInfo.workMinutes * 60);
+  }
+
+  useEffect (() => {
+    initTimer();
+
+    const interval =  setInterval(() => {
+      if (isPausedRef.current) {
+        return;
+      }
+
+      if (secondsLeftRef.current === 0) {
+        return switchMode();
+      }
+
+      tick();
+
+    }, 1000);
+
+    return () => clearInterval(interval);
+  },  [settingsInfo]);
+
+  const totalSeconds = mode === 'work' ? settingsInfo.workMinutes * 60 : settingsInfo.breakMinutes * 60;
+  const percentage = Math.round(secondsLeft / totalSeconds * 100) ;
+
+  const minutes = Math.floor(secondsLeft / 60);
+  let seconds = secondsLeft % 60;
+  if (seconds < 10) seconds = '0' + seconds;
+
+
   return (
     <div>
-      <CircularProgressbar value={44} text={`${46}%`} styles={buildStyles({
+      <CircularProgressbar value={percentage} text={minutes + ':' + seconds} styles={buildStyles({
         textColor: "var(--r7)",
-        pathColor: "var(--r7)",
+        pathColor: mode === 'work' ? "var(--r10)" : "var(--r7)",
         trailColor: "var(--c5)",
       })} />
       <div style={{marginTop: '20px'}}>
-        <PlayButton className={estilo.button} />
-        <PauseButton className={estilo.button} />
-        <SettingsButton className={estilo.button} />
+        {isPaused ? 
+        <PlayButton onClick={() => {setIsPaused(false); isPausedRef.current = false;}} className={estilo.button} /> :
+        <PauseButton onClick={() => {setIsPaused(true); isPausedRef.current = true;}} className={estilo.button} />}
+        
+        
+        <SettingsButton onClick={() => settingsInfo.setShowSettings(true)} className={estilo.button} />
       </div>
     </div>
   )
