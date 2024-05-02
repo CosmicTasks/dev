7d72e95aa86a55b4839d6435885296c2b6545391
thekidisalright <dev.alexandresilva@gmail.com>
2024-05-02 15:48:50 -0300

    Refactor PageTasks component to add Tarefa component for displaying tasks in PageTasks.jsx

--- app/src/components/app/PageTasks.jsx
+++ app/src/components/app/PageTasks.jsx
@@ -11,6 +11,7 @@ import {
 } from "@iconscout/react-unicons";
 import { useListaContext } from "../../hooks/useListaContext";
 import { useTaskContext } from "../../hooks/useTaskContext";
+import Tarefa from "./tarefas/Tarefa";
 
 const PageTasks = () => {
   const [sidebar, setSidebar] = useState(true);
@@ -46,11 +47,11 @@ const PageTasks = () => {
             <UilGrid size="18" color="var(--c11)" />
           </div>
         </div>
+        <div className={style.taskWrapper}>
         {tasks && tasks.map((task) => (
-          <div key={task.id} >
-            <h1>{task.nome}</h1>
-          </div>
+          <Tarefa key={task.id} style={style} task={task} tipo="hoje" />
         ))}
+        </div>
         {/* {listas.map((lista) => (
           <div key={lista.id} className={style.lista}>
             <div className={style.listaHeader}>
