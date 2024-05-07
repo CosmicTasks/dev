09b5f5887eab584fa7e55bed4f609047a3b8bea3
thekidisalright <dev.alexandresilva@gmail.com>
2024-05-07 16:32:24 -0300

    concluindo tarefas

--- app/src/components/app/PageTasks.jsx
+++ app/src/components/app/PageTasks.jsx
@@ -25,6 +25,24 @@ const PageTasks = ({ tipo }) => {
   const [lista, setLista] = useState(null);
   const [tipoNome, setTipoNome] = useState(null);
 
+  const concluirTarefa = async (id) => {
+    const response = await fetch(`http://localhost:4000/api/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        status: "Concluída",
+      }),
+    });
+    if (response.ok) {
+      const data = await response.json();
+      dispatchTasks({ type: "REMOVE_TASK", payload: data });
+    } else {
+      console.log("Erro ao concluir tarefa");
+    }
+  }
+
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"));
 
@@ -143,6 +161,7 @@ const PageTasks = ({ tipo }) => {
                 task={task}
                 setTask={setTarefaSelecionada}
                 valor={tarefaSelecionada}
+                concluir={concluirTarefa}
               />
             ))}
         </div>
@@ -151,6 +170,7 @@ const PageTasks = ({ tipo }) => {
         <ContainerTask
           task={tarefaSelecionada}
           setTask={setTarefaSelecionada}
+          concluir={concluirTarefa}
         />
       )}
     </>
