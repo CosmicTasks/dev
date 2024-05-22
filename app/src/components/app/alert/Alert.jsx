3dd1bb86322a6565eae1ea39eadd25f4f940bb03
dev.alexandresilva <dev.alexandresilva@gmail.com>
2024-05-22 06:33:11 +0000

    alert criado

---
+++ app/src/components/app/alert/Alert.jsx
@@ -0,0 +1,30 @@
+import { UilExclamationTriangle } from "@iconscout/react-unicons";
+import { useEffect, useState } from "react";
+import style from "./Alert.module.css";
+
+const Alert = ({ icon, conteudo, setErro }) => {
+  const [icone, setIcone] = useState(null);
+
+  const click = () => {
+    setErro ? setTimeout(() => setErro(null), 2000) : ''
+  };
+
+  useEffect(() => {
+    switch (icon) {
+      case "erro":
+        setIcone(<UilExclamationTriangle color="var(--c1)" />);
+        break;
+      default:
+        setIcone(<UilExclamationTriangle color="var(--c1)" />);
+    }
+  }, [icon]);
+
+  return (
+    <div className={style.alert} onClick={click}>
+      {icone}
+      <span className={style.title}>{conteudo}</span>
+    </div>
+  );
+};
+
+export default Alert;
