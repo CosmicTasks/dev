ced3f0a0c1fc1240b172a2e3eba59889fbbf0938
mthvalsz <133716976+mthvalsz@users.noreply.github.com>
2024-05-28 22:50:14 -0300

    Inicioado o DashBoard

--- app/src/components/app/dashboard/PageDash.jsx
+++ app/src/components/app/dashboard/PageDash.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { LineChart } from '@mui/x-charts/LineChart';
  const PageDash = () => {
   return (
     <div>
@@ -6,11 +7,11 @@ import React from "react";
         xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
         series={[
           {
-            data: [2, 5.5, 2, 8.5, 1.5, 5],
+            data: [7, 5.5, 2, 8.5, 1.5, 5],
           },
         ]}
-        width={500}
-        height={300}
+        width={1000}
+        height={500}
       />
     </div>
   );
