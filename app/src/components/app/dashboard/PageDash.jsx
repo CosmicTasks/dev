3a41f7084609c6937f070842e20b6dee564b0f6b
thekidisalright <dev.alexandresilva@gmail.com>
2024-06-05 05:12:38 -0300

    refactor: Update PageDash component to use BarChart instead of LineChart

--- app/src/components/app/dashboard/PageDash.jsx
+++ app/src/components/app/dashboard/PageDash.jsx
@@ -1,20 +1,86 @@
-import React from "react";
-import { LineChart } from '@mui/x-charts/LineChart';
- const PageDash = () => {
+import * as React from "react";
+import { BarChart } from "@mui/x-charts/BarChart";
+import { axisClasses } from "@mui/x-charts/ChartsAxis";
+
+const PageDash = () => {
+  const chartSetting = {
+    yAxis: [
+      {
+        label: "quantidade (un)",
+      },
+    ],
+    width: 1000,
+    height: 500,
+    sx: {
+      [`.${axisClasses.left} .${axisClasses.label}`]: {
+        transform: "translate(-20px, 0)",
+      },
+    },
+  };
+  const dataset = [
+    {
+      tarefas: 59,
+      month: "Jan",
+    },
+    {
+      tarefas: 49,
+      month: "Fev",
+    },
+    {
+      tarefas: 40,
+      month: "Mar",
+    },
+    {
+      tarefas: 54,
+      month: "Abr",
+    },
+    {
+      tarefas: 27,
+      month: "Mai",
+    },
+    {
+      tarefas: 60,
+      month: "Jun",
+    },
+    {
+      tarefas: 15,
+      month: "Jul",
+    },
+    {
+      tarefas: 65,
+      month: "Ago",
+    },
+    {
+      tarefas: 51,
+      month: "Set",
+    },
+    {
+      tarefas: 60,
+      month: "Out",
+    },
+    {
+      tarefas: 67,
+      month: "Nov",
+    },
+    {
+      tarefas: 61,
+      month: "Dez",
+    },
+  ];
+
+  const valueFormatter = (value) => `${value}`;
+
   return (
-    <div>
-      <LineChart
-        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-        series={[
-          {
-            data: [7, 5.5, 2, 8.5, 1.5, 5],
-          },
-        ]}
-        width={1000}
-        height={500}
-      />
-    </div>
+    <BarChart
+      dataset={dataset}
+      xAxis={[{ scaleType: "band", dataKey: "month" }]}
+      series={[
+        { dataKey: "tarefas", label: "Tarefas", valueFormatter },
+      ]}
+      {...chartSetting}
+      style={{ fontWeight: "bold"}}
+    />
   );
 };
 
-export default PageDash
+export default PageDash;
