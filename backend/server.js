645cae9426fae81d69a58e80697abab7a62e984a
thekidisalright <dev.alexandresilva@gmail.com>
2024-06-11 00:38:28 -0300

    refactor: Update server.js to log the original URL in addition to method and path

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cors({
 // Middlewares
 app.use(express.json());
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path}`);
+  console.log(`${req.method} ${req.path} ${req.originalUrl}`);
   next();
 });
 
