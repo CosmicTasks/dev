f4641803201bc686ea001c05197f5cae87e6f739
thekidisalright <dev.alexandresilva@gmail.com>
2024-06-18 15:35:38 -0300

    refactor: Update PageDash component to fetch and display weekly tasks data using Chart.js

--- app/src/components/app/dashboard/PageDash.jsx
+++ app/src/components/app/dashboard/PageDash.jsx
@@ -1,85 +1,104 @@
-import * as React from "react";
-import { BarChart } from "@mui/x-charts/BarChart";
-import { axisClasses } from "@mui/x-charts/ChartsAxis";
+import { useEffect, useRef, useState } from "react";
+import Chart from "chart.js/auto";
+import style from "./PageDash.module.css";
 
 const PageDash = () => {
-  const chartSetting = {
-    yAxis: [
-      {
-        label: "quantidade (un)",
-      },
-    ],
-    width: 1000,
-    height: 500,
-    sx: {
-      [`.${axisClasses.left} .${axisClasses.label}`]: {
-        transform: "translate(-20px, 0)",
-      },
-    },
-  };
-  const dataset = [
-    {
-      tarefas: 59,
-      month: "Jan",
-    },
-    {
-      tarefas: 49,
-      month: "Fev",
-    },
-    {
-      tarefas: 40,
-      month: "Mar",
-    },
-    {
-      tarefas: 54,
-      month: "Abr",
-    },
-    {
-      tarefas: 27,
-      month: "Mai",
-    },
-    {
-      tarefas: 60,
-      month: "Jun",
-    },
-    {
-      tarefas: 15,
-      month: "Jul",
-    },
-    {
-      tarefas: 65,
-      month: "Ago",
-    },
-    {
-      tarefas: 51,
-      month: "Set",
-    },
-    {
-      tarefas: 60,
-      month: "Out",
-    },
-    {
-      tarefas: 67,
-      month: "Nov",
-    },
-    {
-      tarefas: 61,
-      month: "Dez",
-    },
-  ];
+  const chartRef = useRef(null);
+  const [tabSemana, setTabSemana] = useState(true);
+  const [data, setData] = useState([]);
+  const [labels, setLabels] = useState([]);
 
-  const valueFormatter = (value) => `${value}`;
+  useEffect(() => {
+    if (tabSemana) {
+      try {
+        const fetchTasks = async () => {
+          const user = JSON.parse(localStorage.getItem("user"));
+          const response = await fetch(
+            `http://localhost:4000/api/tasks/dash/${user._id}?tipo=semana`,
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          const data = await response.json();
+          if (response.ok) {
+            setLabels([
+              "Segunda",
+              "Terça",
+              "Quarta",
+              "Quinta",
+              "Sexta",
+              "Sábado",
+              "Domingo",
+            ]);
+            setData(data);
+            console.log(data);
+          } else {
+            console.log("Erro ao buscar tarefas");
+          }
+        };
+        fetchTasks();
+      } catch (error) {
+        console.log("Erro ao buscar tarefas");
+      }
+    }
+  }, [tabSemana]);
+
+  useEffect(() => {
+    const ctx = document.getElementById("myChart");
+    chartRef.current = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: "Tarefas",
+            data: data,
+            fill: "var(--azul-cerceta)",
+            borderColor: "rgb(75, 192, 192)",
+            tension: 0.1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        animation: {
+          duration: 2000,
+        },
+        elements: {
+          bar: {
+            backgroundColor: "#511abe",
+          },
+        },
+        scales: {
+          y: {
+            beginAtZero: true,
+            ticks: {
+              stepSize: 1,
+            },
+          },
+        },
+      }
+    });
+
+    return () => {
+      chartRef.current.destroy();
+    };
+  }, [data, labels]);
 
   return (
-    <BarChart
-      dataset={dataset}
-      xAxis={[{ scaleType: "band", dataKey: "month" }]}
-      series={[
-        { dataKey: "tarefas", label: "Tarefas", valueFormatter },
-      ]}
-      {...chartSetting}
-      style={{ fontWeight: "bold"}}
-    />
+    <div className={style.body}>
+      {
+        <div className={style.container}>
+          <div className={style.header}>
+            <h1 className={style.title}>Dashboard semanal</h1>
+          </div>
+          <canvas id="myChart"></canvas>
+        </div>
+      }
+    </div>
   );
 };
 
