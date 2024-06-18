3d4ffc9189064b46f60282989cb4ec7cb57d10c7
thekidisalright <dev.alexandresilva@gmail.com>
2024-06-18 16:05:24 -0300

    refactor: Add endpoint to retrieve completed tasks in task routes

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -5,6 +5,7 @@ const {
   buscarTask,
   atualizarTask,
   deletarTask,
+  buscarTasksConcluidas,
 } = require('../controllers/taskController.js');
 
 const router = express.Router();
@@ -14,5 +15,6 @@ router.get('/:id', buscarTasks);
 router.get('/:id', buscarTask);
 router.put('/:id', atualizarTask);
 router.delete('/:id', deletarTask);
+router.get('/dash/:id', buscarTasksConcluidas);
 
 module.exports = router;
