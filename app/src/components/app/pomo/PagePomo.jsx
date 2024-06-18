3f29fdd41dcfd2a1345b3ac049b195bf9b52afdd
thekidisalright <dev.alexandresilva@gmail.com>
2024-06-18 16:04:48 -0300

    refactor: Update PagePomo component to set default work and break minutes, and add anotacao feature

--- app/src/components/app/pomo/PagePomo.jsx
+++ app/src/components/app/pomo/PagePomo.jsx
@@ -1,35 +1,46 @@
-import style from './PagePomo.module.css'
-import Timer from './Timer'
-import Settings from './Settings'
-import { useState } from 'react'
-import SettingsContext from './SettingsContext'
+import style from "./PagePomo.module.css";
+import Timer from "./Timer";
+import Settings from "./Settings";
+import { useState, useEffect } from "react";
+import SettingsContext from "./SettingsContext";
 const PagePomo = () => {
-  
-
-
   const [showSettings, setShowSettings] = useState(false);
-  const [workMinutes, setWorkMinutes] = useState(45);
-  const [breakMinutes, setBreakMinutes] = useState(15);
+  const [workMinutes, setWorkMinutes] = useState(20);
+  const [breakMinutes, setBreakMinutes] = useState(5);
+  const [anotacao, setAnotacao] = useState(
+    localStorage.getItem("anotacao") || ""
+  );
+
+  useEffect(() => {
+    localStorage.setItem("anotacao", anotacao);
+  }, [anotacao]);
 
   return (
     <div className={style.pagePomo}>
       <div className={style.pomoBody}>
-        <SettingsContext.Provider value={{
-          showSettings,
-          setShowSettings,
-          workMinutes,
-          breakMinutes,
-          setWorkMinutes,
-          setBreakMinutes,
-        }}>
-          {showSettings ? <Settings/> :  <Timer estilo={style} />}
+        <SettingsContext.Provider
+          value={{
+            showSettings,
+            setShowSettings,
+            workMinutes,
+            breakMinutes,
+            setWorkMinutes,
+            setBreakMinutes,
+          }}
+        >
+          {showSettings ? <Settings /> : <Timer estilo={style} />}
         </SettingsContext.Provider>
-        
-        
-        
+      </div>
+      <div className={style.divAnotacoes}>
+        <textarea
+          className={style.anotacao}
+          placeholder="Use este espaço para anotações de baixa prioridade"
+          value={anotacao}
+          onChange={(e) => setAnotacao(e.target.value)}
+        ></textarea>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PagePomo
+export default PagePomo;
